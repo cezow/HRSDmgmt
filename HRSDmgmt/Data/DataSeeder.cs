eee9f2cb05321db9aa4bfb6f57b79d9d3fae003f
cez <czarek@interia.eu>
2022-11-22 22:46:05 +0100

    update

--- HRSDmgmt/Data/DataSeeder.cs
+++ HRSDmgmt/Data/DataSeeder.cs
@@ -47,16 +47,16 @@ namespace HRSDmgmt.Data
             {
                 roleStore.CreateAsync(new IdentityRole
                 {
-                    Name = "user",
-                    NormalizedName = "user"
+                    Name = "company",
+                    NormalizedName = "company"
                 }).Wait();
             }
             if (!dbContext.Roles.Any(r => r.Name == "employee"))
             {
                 roleStore.CreateAsync(new IdentityRole
                 {
-                    Name = "user",
-                    NormalizedName = "user"
+                    Name = "employee",
+                    NormalizedName = "employee"
                 }).Wait();
             }
         }
@@ -219,42 +219,42 @@ namespace HRSDmgmt.Data
                             
                         };
                         dbContext.Set<Models.Offer>().Add(offer);
-                    }
-                    dbContext.SaveChanges();
-
-                    /* var idUzytkownika2 = dbContext.AppUsers.Where(u => u.UserName == "autor2@portal.pl").FirstOrDefault().Id;
-
-                    for (int j = 5; j <= 9; j++) //teksty autora2
-                    {
-                        var tekst = new Models.Text()
-                        {
-                            Title = "Tytuł" + i.ToString() + j.ToString(),
-                            Summary = "Streszczenie tekstu o tytule Title" + i.ToString() + j.ToString(),
-                            Keywords = "tag" + j.ToString() + ", tag" + (i + j).ToString(),
-                            Content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor" +
-                                " incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam, quis nostrud exercitation" +
-                                " ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit" +
-                                " in voluptate velit esse cillum dolore eu fugiat nulla pariatur.Excepteur sint occaecat cupidatat" +
-                                " non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Sed ut perspiciatis" +
-                                " unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam," +
-                                " eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo." +
-                                " Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur" +
-                                " magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum" +
-                                " quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut" +
-                                " labore et dolore magnamaliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem" +
-                                " ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure" +
-                                " reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem" +
-                                " eum fugiat quo voluptas nulla pariatur ? ",
-                            AddedDate = DateTime.Now.AddDays(-i * j),
-                            CategoryId = i,
-                            Id = idUzytkownika2,
-                            Active = true
-                        };
-                        dbContext.Set<Models.Text>().Add(tekst);
-                    }
-                    dbContext.SaveChanges(); */
                 }
+                dbContext.SaveChanges();
+
+                /* var idUzytkownika2 = dbContext.AppUsers.Where(u => u.UserName == "autor2@portal.pl").FirstOrDefault().Id;
+
+                for (int j = 5; j <= 9; j++) //teksty autora2
+                {
+                    var tekst = new Models.Text()
+                    {
+                        Title = "Tytuł" + i.ToString() + j.ToString(),
+                        Summary = "Streszczenie tekstu o tytule Title" + i.ToString() + j.ToString(),
+                        Keywords = "tag" + j.ToString() + ", tag" + (i + j).ToString(),
+                        Content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor" +
+                            " incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam, quis nostrud exercitation" +
+                            " ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit" +
+                            " in voluptate velit esse cillum dolore eu fugiat nulla pariatur.Excepteur sint occaecat cupidatat" +
+                            " non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Sed ut perspiciatis" +
+                            " unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam," +
+                            " eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo." +
+                            " Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur" +
+                            " magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum" +
+                            " quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut" +
+                            " labore et dolore magnamaliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem" +
+                            " ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure" +
+                            " reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem" +
+                            " eum fugiat quo voluptas nulla pariatur ? ",
+                        AddedDate = DateTime.Now.AddDays(-i * j),
+                        CategoryId = i,
+                        Id = idUzytkownika2,
+                        Active = true
+                    };
+                    dbContext.Set<Models.Text>().Add(tekst);
+                }
+                dbContext.SaveChanges(); */
             }
         }
     }
+    
 }
