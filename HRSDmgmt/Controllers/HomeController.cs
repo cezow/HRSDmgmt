3329a0c25658a2e36a280f3a2de410428608ba42
cez <czarek@interia.eu>
2022-12-01 20:55:14 +0100

    update

--- HRSDmgmt/Controllers/HomeController.cs
+++ HRSDmgmt/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
-﻿using HRSDmgmt.Models;
+﻿using HRSDmgmt.Data;
+using HRSDmgmt.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace HRSDmgmt.Controllers
@@ -7,10 +9,12 @@ namespace HRSDmgmt.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context; 
         }
 
         public IActionResult Index()
@@ -20,6 +24,19 @@ namespace HRSDmgmt.Controllers
 
         public IActionResult Privacy()
         {
+            return View();
+        }
+
+        public async Task<IActionResult> Dashboard()
+        {
+            var companies = await _context.Companies.ToListAsync();
+            var offers = await _context.Offers.ToListAsync();
+            var employees = await _context.Employees.ToListAsync();
+
+            ViewBag.Companies = companies.Count();
+            ViewBag.Offers = offers.Count();
+            ViewBag.Employees = employees.Count();
+
             return View();
         }
 
